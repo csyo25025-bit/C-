242d4fb8684815c2363fd1e357bd62d1639420c2
csyo25025-bit <csyo25025@g.nihon-u.ac.jp>
2026-08-10 18:54:00 +0900

    10進数から8進数へ変換

---
+++ 10進数から8進数/10進数から8進数.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	const int bit_num = 16;
+	int dec, oct[bit_num];
+	int tmp, i;
+
+	for (i = 0; i < bit_num; ++i) {
+	
+		oct[i] = 0;
+	
+	}
+
+	cout << "input decimal:";
+	cin >> dec;
+	tmp = dec;
+	i = 0;
+
+	while (tmp > 0 && i < bit_num) {
+	
+		oct[i] = tmp % 8;
+		tmp /= 8;
+		++i;
+	
+	}
+
+	cout << "(" << dec << ")10=(";
+
+	for (int j = bit_num - 1; j >= 0; --j) {
+	
+		cout << oct[j];
+	
+	}
+
+	cout << ")8" << endl;
+	return 0;
+
+}
