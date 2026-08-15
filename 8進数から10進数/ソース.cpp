86cc8ee3689663a3b89b7eb55d11e8c9caf30fac
csyo25025-bit <csyo25025@g.nihon-u.ac.jp>
2026-08-15 11:59:22 +0900

    8進数から10進数に変換するプログラムの実装
    入力された8進数を10進数に変換するプログラムの実装。

---
+++ 8進数から10進数/ソース.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	string OCT;
+	int dec = 0;
+	int base = 8;
+
+	cout << "input octal" << endl;
+	cin >> OCT;
+	string tmp = OCT;
+
+	int ex = OCT.length();
+
+	for (int i = 0; i < ex; ++i) {
+
+		if (OCT[i] != '0' && OCT[i] != '1' && OCT[i] != '2' && OCT[i] != '3' && OCT[i] != '4' && OCT[i] != '5' && OCT[i] != '6' && OCT[i] != '7') {
+
+			cout << "八進数以外の文字が入力されている" << endl;
+			return 1;
+
+		}
+
+		dec = dec * base + (OCT[i] - '0');
+
+	}
+
+	cout << "(" << tmp << ")" << base << "=(" << dec << ")10";
+	return 0;
+
+}
