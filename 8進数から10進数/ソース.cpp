e2c38373eab41e61ee7329cfc7b39785b912ee8c
csyo25025-bit <csyo25025@g.nihon-u.ac.jp>
2026-08-15 12:14:44 +0900

    進数変換の入力チェックを動的に変更可能に修正
    文字の比較判定を文字コードの計算を利用する形に変更。
    今後進数変更時にコードの修正なしで対応可能。

--- 8進数から10進数/ソース.cpp
+++ 8進数から10進数/ソース.cpp
@@ -7,6 +7,7 @@ int main() {
 	string OCT;
 	int dec = 0;
 	int base = 8;
+	int dis = base - 1;
 
 	cout << "input octal" << endl;
 	cin >> OCT;
@@ -16,7 +17,7 @@ int main() {
 
 	for (int i = 0; i < ex; ++i) {
 
-		if (OCT[i] != '0' && OCT[i] != '1' && OCT[i] != '2' && OCT[i] != '3' && OCT[i] != '4' && OCT[i] != '5' && OCT[i] != '6' && OCT[i] != '7') {
+		if (OCT[i] < '0' || OCT[i]>dis + '0') {
 
 			cout << "八進数以外の文字が入力されている" << endl;
 			return 1;
