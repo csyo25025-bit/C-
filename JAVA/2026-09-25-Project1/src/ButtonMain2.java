b74bbeabd5c62a2514200221a082bd72f5e4f78d
csyo25025-bit <csyo25025@g.nihon-u.ac.jp>
2026-09-25 14:29:43 +0900

    追加課題の作成
    1秒当たりのボタンが押された回数をボタンの下に表示されるようにした。

--- JAVA/2026-09-25-Project1/src/ButtonMain2.java
+++ JAVA/2026-09-25-Project1/src/ButtonMain2.java
@@ -14,7 +14,9 @@ public class ButtonMain2 extends JFrame implements ActionListener{
 	private static final long serialVersionUID = 1L;
 	JButton btn;
 	JLabel lb;
+	JLabel rb;
 	int count=0;
+	long TIME;
 
 	public static void main(String[] args) {
 		ButtonMain2 w = new ButtonMain2();
@@ -25,15 +27,23 @@ public class ButtonMain2 extends JFrame implements ActionListener{
 		setSize(200, 200); //Window のサイズをセット
 		setTitle("ButtonPressed?"); // Windowのタイトルをセット
 		lb = new JLabel("0"); // テキストフィールド生成
+		rb=new JLabel("0.0");//テキストフィールド生成
 		btn = new JButton("press me!"); // ボタン生成
 		btn.addActionListener(this); // ボタンが押された時はこのクラスで処理
 		add(lb, BorderLayout.NORTH); //　テキストフィールドを北(上)に配置
+		add(rb,BorderLayout.SOUTH);//テキストフィールド南（下）に配置
 		add(btn, BorderLayout.CENTER); // ボタンを中央に配置
+		TIME=System.currentTimeMillis();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent ae) {
 		++count;//ボタンを押した回数
+		long now=System.currentTimeMillis();//今の時間
+		double time=(now-TIME)/1000.0;//経過時間（秒）
+		double COUNT=count/time;//一秒あたりの回数
+		
 		lb.setText(Integer.toString(count));
+		rb.setText(String.format("%.2f",COUNT));
 	}
 }
